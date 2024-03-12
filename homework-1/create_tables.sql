9d2d4a47b9479c16c95efc39c7298c545b55a31d
DarkhanDaurenov <147601220+DarkhanDaurenov@users.noreply.github.com>
2024-03-12 15:20:24 +0600

    Update create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date varchar(50) NOT NULL,
+	notes text NOT NULL
+);
+
+SELECT * FROM orders;
+
+CREATE TABLE customers
+(
+	customer_id varchar(50) PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL 
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
